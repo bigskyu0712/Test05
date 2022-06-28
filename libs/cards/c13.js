442bab2ac19bf13278e4daf4d07a777b53e1d501
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-06-28 18:05:36 +0900

    Update c13.js

--- libs/cards/c13.js
+++ libs/cards/c13.js
@@ -1,22 +1,31 @@
-const Card = require('./card.js');
-//�J�[�h��2�ԖځC�A�C�e���̎擾�ɂȂ�܂��B
+
+const Card = require("./card.js");
+//c13.js
+//カードの13番目，全員がそれぞれの手札からカードを1枚選んで捨てる
 
 
 module.exports = class c13 extends Card {
 
-    //�J�[�h�^�C�v�ݒ�C
+    //カードタイプ設定，
     static cardType = 1;
 
-    //�R���X�g���N�^�C�K�v���Ȃ���΂�����Ȃ��đ��v�ł��D
-    constructor(board,player) {
+    //コンストラクタ，必要がなければいじらなくて大丈夫です．
+    constructor(board,player,cardId) {
         super(board,player);
+        this.cardId = cardId;
     }
 
-    //�������L�q
+    //処理を記述
     effect(){
-        board.addItemRandom(player);
-        console.log('13');
+        //全員の選択をどう受け取るか
+        //誰がどのカードを選んだかの管理
+        //分からない
+        board.everyoneDeleteCard(player, cardId);
     }
 
+    //クライアントから送信後データを受け取った時
+    afterEffect(){
+        console.log("after");
+    }
 
 }
