2e031a874fe26e960533384056c4cc051eed720a
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-03 19:18:44 +0900

    Update c12.js

--- libs/cards/c12.js
+++ libs/cards/c12.js
@@ -1,7 +1,13 @@
+/*******************************************************************
+***  File Name          : c12.js
+***  Version            : V1.0
+***  Designer           : 武田 和大
+***  Date               : 2022.07.02
+***  Purpose            : 自分の手札からカードを1枚選んで捨てる
+***
+*******************************************************************/
 
 const Card = require("./card.js");
-//c12.js
-//カードの12番目，自分の手札からカードを1枚選んで捨てる
 
 
 module.exports = class c12 extends Card {
@@ -10,24 +16,25 @@ module.exports = class c12 extends Card {
     static cardType = 1;
 
     //コンストラクタ，必要がなければいじらなくて大丈夫です．
-    constructor(board,player,cardId) {
+    constructor(board,player) {
         super(board,player);
-        this.cardId = cardId;
     }
 
     //処理を記述
     effect(){
-        board.deleteCard(player, cardId); 
-    }
-
-    //board
-    deleteCard(player, cardId){
-        player.deleteCard(cardId);
+        this.board.selectCardFromHand(this.player, 1);
     }
 
     //クライアントから送信後データを受け取った時
-    afterEffect(){
-        console.log("after");
+    afterEffect(data){
+        console.log("12");
+        this.board.deleteCard(this.player, data.cardId);
     }
+
+    //カードIdからカードを消去
+    //board
+    /*deleteCard(player, cardId){
+        player.deleteCard(cardId);
+    }*/
 
 }
