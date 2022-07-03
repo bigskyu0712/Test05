4f3be04a5d130a3f21356a0e906545f9cbb9fd2b
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-03 19:22:20 +0900

    Update c18.js

--- libs/cards/c18.js
+++ libs/cards/c18.js
@@ -1,7 +1,13 @@
+/*******************************************************************
+***  File Name          : c18.js
+***  Version            : V1.0
+***  Designer           : 武田 和大
+***  Date               : 2022.07.02
+***  Purpose            : 自分の後ろの番の人と位置を入れ替える
+***
+*******************************************************************/
 
 const Card = require("./card.js");
-
-//カードの18番目，自分の後ろの番の人と位置を入れ替える
 
 module.exports = class c18 extends Card {
 
@@ -9,15 +15,15 @@ module.exports = class c18 extends Card {
     static cardType = 1;
 
     //コンストラクタ，必要がなければいじらなくて大丈夫です．
-    constructor(board,turn) {
-        this.board = board;
-        this.turn = turn;
+    constructor(board,player) {
+        super(board,player);
     }
 
     //処理を記述
     effect(){
-        next = board.getNextUser(turn);
-        board.changePosition(board.players[turn], board.players[next]); 
+        let turn = this.board.getTurn(); //c17
+        let next = (turn + 1) % this.players.length;
+        this.board.changePosition(this.board.players[turn], this.board.players[next]); 
     }
 
     //changePositionはc30
