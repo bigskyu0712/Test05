834bf0d4acf345a07fd079a27d05f18fd228c7ff
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-03 19:22:53 +0900

    Update c20.js

--- libs/cards/c20.js
+++ libs/cards/c20.js
@@ -1,7 +1,14 @@
+/*******************************************************************
+***  File Name          : c20.js
+***  Version            : V1.0
+***  Designer           : 武田 和大
+***  Date               : 2022.07.02
+***  Purpose            : もう1度サイコロを振ることが出来る
+***
+*******************************************************************/
 
 const Card = require("./card.js");
 
-//カードの20番目，もう1度サイコロを振ることが出来る
 
 module.exports = class c20 extends Card {
 
@@ -15,7 +22,25 @@ module.exports = class c20 extends Card {
 
     //処理を記述
     effect(){
-        //サイコロ振る処理？と進む処理
+        //サイコロ振る処理
+        let dice = this.board.diceDueToCard(this.player) 
+        //進む処理
+        this.board.moveDueToCard(this.player, dice);  
     }
+
+    //サイコロを振る
+    //board
+    /*diceDueToCard(player){
+        this.diceNum = Math.floor(Math.random() * 6) + 1;
+        display.dice(player.getUserId(), this.diceNum);
+    }*/
+
+    //位置を更新し、コマが動く
+    /*moveDueToCard(player, dice){
+        console.log("position=" + player.getPosition() + "dice=" + dice);
+        player.updatePosition(dice);
+        display.updatePosition(this.roomId, turn, player.getPosition());
+        console.log("position=" + player.getPosition());
+    }*/
 
 }
