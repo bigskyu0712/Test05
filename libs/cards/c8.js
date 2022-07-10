079fb068ecff94d07a123aa8f191237696d35590
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-10 22:28:14 +0900

    c8のコメント追加しました。

--- libs/cards/c8.js
+++ libs/cards/c8.js
@@ -20,7 +20,14 @@ module.exports = class c8 extends Card {
         super(board,player);
     }
 
-    //処理を記述
+/******************************************************************
+*** Method Name         : effect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : カードの効果を適応する
+*** Return              : なし
+******************************************************************/
+
     effect(){
         this.board.everyoneDeleteRandom(); //deleteRandomを全員分行う
     }
