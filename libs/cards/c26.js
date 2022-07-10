436ebea62f18e2468ae1157ff4e7f3a1bfef2b4d
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-10 22:56:57 +0900

    c26のコメント追加しました。

--- libs/cards/c26.js
+++ libs/cards/c26.js
@@ -16,16 +16,34 @@ module.exports = class c26 extends Card {
     static cardType = 1;
 
     //コンストラクタ，必要がなければいじらなくて大丈夫です．
-    constructor(board,player) {
+    constructor(board,   //boardクラス
+                player)  //playerクラス
+    {
         super(board,player);
     }
 
-    //処理を記述
+/******************************************************************
+*** Method Name         : effect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : カードの効果を適応する
+*** Return              : なし
+******************************************************************/
+
     effect(){
         board.selectPlayer(this.player, 1);
     }
 
-    afterEffect(data){
+/******************************************************************
+*** Method Name         : afterEffect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : クライアントから送信後データを受け取った後の処理
+*** Return              : なし
+******************************************************************/
+
+    afterEffect(data)   //sendData
+    {
         console.log("26");
         board.changeAllcard(this.player, data.selectPlayer);
     }
