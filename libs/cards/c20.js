f69b7feb1177775110aaedb1f5bcc95facf82348
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-10 22:47:37 +0900

    c20のコメント追加しました。

--- libs/cards/c20.js
+++ libs/cards/c20.js
@@ -16,15 +16,32 @@ module.exports = class c20 extends Card {
     static cardType = 1;
 
     //コンストラクタ，必要がなければいじらなくて大丈夫です．
-    constructor(board,player) {
+    constructor(board,   //boardクラス
+                player)  //playerクラス
+    {
         super(board,player);
     }
 
-    //処理を記述
+/******************************************************************
+*** Method Name         : effect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : カードの効果を適応する
+*** Return              : なし
+******************************************************************/
+
     effect(){
         //サイコロ振る処理
         this.board.nonAction(this.player);
     }
+
+/******************************************************************
+*** Method Name         : afterEffect()
+*** Designer            : 曾根 悠太
+*** Date                : 2022.07.04
+*** Method              : クライアントから送信後データを受け取った後の処理
+*** Return              : なし
+******************************************************************/
 
     afterEffect(){
         this.board.game.gameState = 2;
