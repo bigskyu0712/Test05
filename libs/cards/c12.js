1e7f6d84aaa11a029f78a591aba93271dfd5905b
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-10 22:41:11 +0900

    c12のコメント追加しました。

--- libs/cards/c12.js
+++ libs/cards/c12.js
@@ -16,16 +16,32 @@ module.exports = class c12 extends Card {
     static cardType = 1;
 
     //コンストラクタ，必要がなければいじらなくて大丈夫です．
-    constructor(board,player) {
+    constructor(board,   //boardクラス
+                player)  //playerクラス
+    {
         super(board,player);
     }
 
-    //処理を記述
+/******************************************************************
+*** Method Name         : effect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : カードの効果を適応する
+*** Return              : なし
+******************************************************************/
+
     effect(){
         this.board.selectCardFromHand(this.player, 1);
     }
 
-    //クライアントから送信後データを受け取った時
+/******************************************************************
+*** Method Name         : afterEffect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : クライアントから送信後データを受け取った後の処理
+*** Return              : なし
+******************************************************************/
+
     afterEffect(data){
         console.log("12");
         this.board.deleteCard(this.player, data.cardId);
