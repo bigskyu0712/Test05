07be92d6786cb6e32b8f6506f1250f3d1ad5686d
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-10 22:57:42 +0900

    c28のコメント追加しました。

--- libs/cards/c28.js
+++ libs/cards/c28.js
@@ -18,11 +18,20 @@ module.exports = class c28 extends Card {
     static cardType = 1;
 
     //コンストラクタ，必要がなければいじらなくて大丈夫です．
-    constructor(board,player) {
+    constructor(board,   //boardクラス
+                player)  //playerクラス
+    {
         super(board,player);
     }
 
-    //処理を記述
+/******************************************************************
+*** Method Name         : effect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : カードの効果を適応する
+*** Return              : なし
+******************************************************************/
+
     effect(){
         this.board.deleteAllItem(this.player); //deleteItemRandomを所持数分回す？
     }
