45092d566d71abc2e9d31a899c90907c3af2f980
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-10 22:27:30 +0900

    c7のコメント追加しました。

--- libs/cards/c7.js
+++ libs/cards/c7.js
@@ -20,7 +20,14 @@ module.exports = class c7 extends Card {
         super(board,player);
     }
 
-    //処理を記述
+/******************************************************************
+*** Method Name         : effect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : カードの効果を適応する
+*** Return              : なし
+******************************************************************/
+
     effect(){
         this.board.deleteAllCard(this.player); //deleteCardRandomを所持数分回す
     }
