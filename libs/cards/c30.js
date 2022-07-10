071d7067043e1c82aa1688e6cba99b959b92dfbb
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-10 22:58:38 +0900

    c30のコメント追加しました。

--- libs/cards/c30.js
+++ libs/cards/c30.js
@@ -16,16 +16,34 @@ module.exports = class c30 extends Card {
     static cardType = 1;
 
     //コンストラクタ，必要がなければいじらなくて大丈夫です．
-    constructor(board,player) {
+    constructor(board,   //boardクラス
+                player)  //playerクラス
+    {
         super(board,player);
     }
 
-    //処理を記述
+/******************************************************************
+*** Method Name         : effect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : カードの効果を適応する
+*** Return              : なし
+******************************************************************/
+
     effect(){
         this.board.selectPlayer(this.player, 1);
     }
 
-    afterEffect(data){
+/******************************************************************
+*** Method Name         : afterEffect()
+*** Designer            : 武田 和大
+*** Date                : 2022.07.04
+*** Method              : クライアントから送信後データを受け取った後の処理
+*** Return              : なし
+******************************************************************/
+
+    afterEffect(data)   //sendData
+    {
         console.log("30");
         this.board.changePosition(this.player, data.selectPlayer);
     }
