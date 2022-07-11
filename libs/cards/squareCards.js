e2108b1d8ace362e7d930cce0306248b798ab15c
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-11 23:10:44 +0900

    コメント更新しました。

--- libs/cards/squareCards.js
+++ libs/cards/squareCards.js
@@ -2,18 +2,27 @@
 ***  File Name          : squareCards.js
 ***  Version            : V1.0
 ***  Designer           : 曾根 悠太
-***  Date               : 2022.07.02
-***  Purpose            : 
+***  Date               : 2022.07.04
+***  Purpose            : boardとplyaerオブジェクトの設定をする
 ***
 *******************************************************************/
 
 /*
 *** Revision :
-*** v1.0 : 曾根 悠太, 2022.07.04
+*** v1.0 : 曾根 悠太, 2022.07.02
 */
 
 module.exports = class squareCards {
     
+    
+    /****************************************************************************
+    *** Method Name         : constructor()
+    *** Designer            : 曾根 悠太
+    *** Date                : 2022.07.02
+    *** Method              : boardとplyaerオブジェクトの設定
+    *** Return              : なし
+    ****************************************************************************/
+
     constructor(board,   //boardクラス
                 userId)  //playerのId
     {
