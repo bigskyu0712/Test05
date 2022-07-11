47affe5d527ccbb1b62768fe1f38c59f352e650f
Kazuhiro91 <1019tkdkazuhiri@gmail.com>
2022-07-11 13:12:58 +0900

    squareCardsのコメント追加しました。

--- libs/cards/squareCards.js
+++ libs/cards/squareCards.js
@@ -1,5 +1,22 @@
+/*******************************************************************
+***  File Name          : squareCards.js
+***  Version            : V1.0
+***  Designer           : 曾根 悠太
+***  Date               : 2022.07.02
+***  Purpose            : 
+***
+*******************************************************************/
+
+/*
+*** Revision :
+*** c1.0 : 曾根 悠太, 2022.07.04
+*/
+
 module.exports = class squareCards {
-    constructor(board,userId) {
+    
+    constructor(board,   //boardクラス
+                userId)  //playerのId
+    {
         this.board = board;
         this.player = this.board.getPlayer(userId);
     }
