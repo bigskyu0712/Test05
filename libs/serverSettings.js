45efe7f1613756f8c564cf22f4d4b8524feb13de
kasaten <magic.h.s.taka@gmail.com>
2022-07-11 20:32:07 +0900

    coment out

--- libs/serverSettings.js
+++ libs/serverSettings.js
@@ -1,4 +1,12 @@
-//サーバーの設定クラス
+/*******************************************************************
+***  File Name          : board.js
+***  Version            : V1.1
+***  Designer           : 曾根 悠太
+***  Date               : 2022.07.04
+***  Purpose            : //サーバーの設定クラス
+***
+*******************************************************************/
+
 
 module.exports = class serverSettings{
     static get MAXROOMIDNUMBER(){ return 9999; }
